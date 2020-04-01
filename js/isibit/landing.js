6d503540e6032c1ba2c6762851f86036dd80ebeb
dPeluChe <olimpuz@gmail.com>
2020-04-01 09:40:39 -0600

    landing and form update

--- js/isibit/landing.js
+++ js/isibit/landing.js
@@ -45,54 +45,56 @@ $(document).ready(function() {
             company: form_company,
             country: form_country,
         };
-        console.log(data);
 
-        var xhr = new XMLHttpRequest();
-        xhr.open('POST', 'https://script.google.com/macros/s/AKfycbz5oaeGtQF_KIFNHs2CcqxUTUQ8EYEbzE13_NgK-hk52N0DrSF5/exec', true);
-        xhr.withCredentials = true;
-        xhr.onreadystatechange = function() {
-            if (xhr.readyState === 2) {// do something}
-                console.log("ready");
-            }
-            xhr.setRequestHeader('Content-Type', 'application/json');
-            xhr.send(data);
+    });
+        /*
+                console.log(data);
 
-/*
-        $.ajax({
-                //method: 'POST',
-                type: "POST",
-                //jsonp: "callback",
-                //crossdomain: true,
-                contentType: 'plain/text',
-                dataType: 'json',
-                xhrFields: {
-                    withCredentials: true
-                },
-                //crossOrigin: true,
-                //async: true,
-                data:JSON.stringify(data),
-                url: "https://script.google.com/macros/s/AKfycbz5oaeGtQF_KIFNHs2CcqxUTUQ8EYEbzE13_NgK-hk52N0DrSF5/exec",
-                headers: { 'Access-Control-Allow-Origin': '*' }, //add this line
-                /*headers: {
-                    'Access-Control-Allow-Methods': '*',
-                    "Access-Control-Allow-Credentials": true,
-                    "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization",
-                    "Access-Control-Allow-Origin": "*",
-                    "Control-Allow-Origin": "*",
-                    "cache-control": "no-cache",
-                    'Content-Type': 'application/javascript',
-                    //'Content-Type': 'application/x-www-form-urlencoded',
-                    'Accept': 'application/json',
-                },
-            success: function(response){
-                console.log("Respond was: ", response);
-            },
-            error: function(request, status, error) {
-                console.log("There was an error: ", error);
-            }
-        });
-    */
-    }
+                var xhr = new XMLHttpRequest();
+                xhr.open('POST', 'https://script.google.com/macros/s/AKfycbz5oaeGtQF_KIFNHs2CcqxUTUQ8EYEbzE13_NgK-hk52N0DrSF5/exec', true);
+                xhr.withCredentials = true;
+                xhr.onreadystatechange = function() {
+                    if (xhr.readyState === 2) {// do something}
+                        console.log("ready");
+                    }
+                    xhr.setRequestHeader('Content-Type', 'application/json');
+                    xhr.send(data);
+
+                $.ajax({
+                        //method: 'POST',
+                        type: "POST",
+                        //jsonp: "callback",
+                        //crossdomain: true,
+                        contentType: 'plain/text',
+                        dataType: 'json',
+                        xhrFields: {
+                            withCredentials: true
+                        },
+                        //crossOrigin: true,
+                        //async: true,
+                        data:JSON.stringify(data),
+                        url: "https://script.google.com/macros/s/AKfycbz5oaeGtQF_KIFNHs2CcqxUTUQ8EYEbzE13_NgK-hk52N0DrSF5/exec",
+                        headers: { 'Access-Control-Allow-Origin': '*' }, //add this line
+                        /*headers: {
+                            'Access-Control-Allow-Methods': '*',
+                            "Access-Control-Allow-Credentials": true,
+                            "Access-Control-Allow-Headers" : "Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization",
+                            "Access-Control-Allow-Origin": "*",
+                            "Control-Allow-Origin": "*",
+                            "cache-control": "no-cache",
+                            'Content-Type': 'application/javascript',
+                            //'Content-Type': 'application/x-www-form-urlencoded',
+                            'Accept': 'application/json',
+                        },
+                    success: function(response){
+                        console.log("Respond was: ", response);
+                    },
+                    error: function(request, status, error) {
+                        console.log("There was an error: ", error);
+                    }
+                });
+            */
+
 });
 
 
