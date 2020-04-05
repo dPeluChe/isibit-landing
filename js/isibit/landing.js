cae273655a963b7b251b89f2903c4be86625856a
dPeluChe <olimpuz@gmail.com>
2020-04-05 15:57:02 -0500

    form valid and animations

--- js/isibit/landing.js
+++ js/isibit/landing.js
@@ -2,6 +2,11 @@ var  mn = $(".navbar.navbar-default");
 var  mns = "navbar-fixed-top";
 var  hdr = $('#header').height();
 
+var form;
+
+new WOW().init();
+
+
 $(window).scroll(function() {
     if( $(this).scrollTop() > (hdr+80) ) {
         mn.addClass(mns);
@@ -9,6 +14,7 @@ $(window).scroll(function() {
         mn.removeClass(mns);
     }
 });
+
 
 $('#submitmodal').on('shown.bs.modal', function () {
     $('#firstname').trigger('focus')
@@ -20,10 +26,45 @@ const vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0
 //const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 
 $(document).ready(function() {
+    form = $("#joinbeta");
+
     hide_needs_tabs();
     $("#txt_managers").show();
 
-    $("#joinbeta").validate();
+    form.validate({
+        rules: {
+            firstname : {
+                required: true,
+                minlength: 3
+            },
+            lastname : {
+                required: true,
+                minlength: 3
+            },
+            company : {
+                required: true,
+                minlength: 3
+            },
+            email: {
+                required: true,
+                email: true
+            }
+        },
+        messages : {
+            firstname: {
+                minlength: "Name should be at least 3 characters"
+            },
+            lastname: {
+                minlength: "Name should be at least 3 characters"
+            },
+            company: {
+                minlength: "Name should be at least 3 characters"
+            },
+            email: {
+                email: "The email should be in the format: abc@domain.tld"
+            },
+        }
+    });
 
 
     $("#btn_managers, #btn_teams, #btn_travelers").click(function(){
@@ -34,46 +75,50 @@ $(document).ready(function() {
     });
 
     $( ".btn_beta, .btn_beta_white").click(function() {
-        cleanForm();
+        //cleanForm();
     });
 
     $( "#btn_submit").click(function() {
-        let form_name = $('#name').val();
-        let formlast_name = $('#last_name').val();
-        let form_email = $('#email').val();
-        let form_company = $('#company').val();
-        let form_country = $('#country').val();
+        var complete = form.valid();
+        console.log(complete);
+        if (complete == false)
+            return;
+
+        let f_firstname = $('#firstname').val();
+        let f_lastname = $('#lastname').val();
+        let f_email = $('#email').val();
+        let f_company = $('#company').val();
+        let f_country = $('#country').val();
 
         let data = {
-            first_names : form_name,
-            last_names: formlast_name,
-            company_email: form_email,
-            company: form_company,
-            country: form_country,
+            first_names : f_firstname,
+            last_names: f_lastname,
+            company_email: f_email,
+            company: f_company,
+            country: f_country,
             mobileorweb: brw+"w"+vw,
             os: brw_os
         };
 
         //console.log (jsonData);
         sendData(JSON.stringify(data));
-        $("#thanks_footer").show(500);
-
     });
 });
 
 function cleanForm(){
-    $('#name').val(" ");
-    $('#last_name').val(" ");
-    $('#email').val(" ");
-    $('#company').val(" ");
+    $('#firstname').val('');
+    $('#lastname').val('');
+    $('#email').val('');
+    $('#company').val('');
 }
 
 
 function sendData(jsonData){
     let googleUrl= "https://script.google.com/macros/s/AKfycbz5oaeGtQF_KIFNHs2CcqxUTUQ8EYEbzE13_NgK-hk52N0DrSF5/exec";
     console.log(jsonData);
+    $("#btn_submit").attr('disabled', true);
 
-    /*
+
     var request = $.ajax({
         url: googleUrl,
         type:"POST",
@@ -82,7 +127,10 @@ function sendData(jsonData){
     });
 
     request.done(function (response, textStatus, jqXHR){
-        console.log("Encuesta guardada correctamente!");
+        console.log("Finish Onboarding Process");
+        form.html(" ");
+        $('#f_complete').removeClass("d-none");
+        $('#f_complete').show();
     });
 
     request.fail(function (jqXHR, textStatus, errorThrown){
@@ -95,7 +143,6 @@ function sendData(jsonData){
     request.always(function () {
     });
     event.preventDefault();
-    */
 
 }
 
